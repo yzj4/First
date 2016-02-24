a63c9f9fc0f3a9a185401d3d560ca78dc137eef2
l k j y <yzj19932@sina.com>
2016-02-24 16:13:23 +0800

    xiaoneinihao

---
+++ App/App/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  App
+//
+//  Created by l k j y on 16/2/24.
+//  Copyright © 2016年 尹照俊. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("xiaomeinihao\n");
+    return 0;
+}
