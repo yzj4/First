6786b9cc4c8c56a42e568e3982e174c1499cc4cf
l k j y <yzj19932@sina.com>
2016-02-24 15:21:22 +0800

    hahah

---
+++ as/as/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  as
+//
+//  Created by l k j y on 16/2/24.
+//  Copyright © 2016年 尹照俊. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("hahahh\n");
+    return 0;
+}
